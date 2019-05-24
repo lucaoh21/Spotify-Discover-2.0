7746ab9cf52da3515588e91490a81b1a312f4c41
Luca Ostertag-Hill <ldostert@bowdoin.edu>
2019-05-24 09:45:45 -0400

    config changes

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = b'\x1e\x10\x81v\x00\x06\x7f\x00<Q\xa0\xb3\xe0\xbd,\xf5'
+    client_id = ''
     #os.environ.get('SECRET_KEY') or 'my-super-powerful-unbreakable-key'
