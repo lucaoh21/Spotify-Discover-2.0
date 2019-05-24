b782fd92be248c2e9b3e1b79361a9093d4f7ab19
Luca Ostertag-Hill <ldostert@bowdoin.edu>
2019-05-24 09:44:49 -0400

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config(object):
-    SECRET_KEY = b'\x1e\x10\x81v\x00\x06\x7f\x00<Q\xa0\xb3\xe0\xbd,\xf5'
-    #os.environ.get('SECRET_KEY') or 'my-super-powerful-unbreakable-key'
